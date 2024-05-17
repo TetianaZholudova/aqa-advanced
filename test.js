5885376a321e444d05c82036f975611686177a9c
Tetiana Zholudova <tanya.slov@gmail.com>
2024-05-17 15:49:52 +0200

    New changes

--- test.js
+++ test.js
@@ -92,22 +92,22 @@ const { Console } = require("console");
 //let personAge = 25; 
 //console.log(personAge>=18 ? 'User is adult' :'User is not adult')
 
-//const averageGrade = 100
-//if(averageGrade < 60){
-  //  console.log('Незадовільно')
-//}
-//else if(averageGrade >= 60 && averageGrade <= 70){
-  //  console.log('Задовільно') 
-//}   
-//else if(averageGrade >= 71 && averageGrade <= 80){
-//console.log('Добре') 
-//}   
-//else if(averageGrade >= 81 && averageGrade <= 90){
- //   console.log('Дуже добре') 
-   // } 
-//else if (averageGrade >= 91 && averageGrade <= 100){
-  //      console.log('Відмінно') 
-    //    }
+const averageGrade = 100
+if(averageGrade < 60){
+console.log('Незадовільно')
+}
+else if(averageGrade >= 60 && averageGrade <= 70){
+console.log('Задовільно') 
+}   
+else if(averageGrade >= 71 && averageGrade <= 80){
+console.log('Добре') 
+}   
+else if(averageGrade >= 81 && averageGrade <= 90){
+console.log('Дуже добре') 
+} 
+else if (averageGrade >= 91 && averageGrade <= 100){
+console.log('Відмінно') 
+}
 
     const averageGrade = 67
 
