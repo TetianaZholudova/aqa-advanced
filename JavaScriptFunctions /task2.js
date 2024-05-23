dbb1909b92139a13d8f305fbc04503bf6bc81bc8
Tetiana Zholudova <tanya.slov@gmail.com>
2024-05-23 10:03:21 +0200

    Jira ticket 12002: JavaScript functions

---
+++ JavaScriptFunctions /task2.js
@@ -0,0 +1,12 @@
+function test(age){
+            if (age >= 18)
+                 return true 
+             
+            else 
+            return false
+        } 
+    console.log('Is a person adult?', test(15)); 
+    console.log('Is a person adult?', test(25));
+        
+
+
