ac3089bd1233502c1d6c66628ce164574887a377
Tetiana Zholudova <tanya.slov@gmail.com>
2024-06-06 08:40:05 +0300

    Jira ticket 12005: Objects_in_JavaScript

---
+++ Objects_in_Java/task4.js
@@ -0,0 +1,12 @@
+
+const person = {
+    firstName: 'Anthony',
+    lastName: 'Pope',
+    age: 45
+  };
+  
+  person.email = 'anthony.popem@example.com';
+  
+  delete person.age;
+  
+  console.log(person);
